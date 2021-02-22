bbd0860c017d86f3cc700fd34cdaefef6ae0e3e9
Zahoor Mohamed <zahoor@ethswarm.org>
2021-02-22 13:04:07 +0530

    add timestamp to all tables and create new counters table

--- createdb.py
+++ createdb.py
@@ -10,20 +10,44 @@ conn.execute('''CREATE TABLE IPTOCITY
           IP             TEXT     NOT NULL,
           LAT            REAL     NOT NULL,
           LNG            REAL     NOT NULL,
-          CITY           TEXT     NOT NULL);''')
+          CITY           TEXT     NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
 conn.execute('''CREATE INDEX iptocity_ip on IPTOCITY(IP)''')
 print("Table IPTOCITY created successfully");
 
 conn.execute('''CREATE TABLE OVERLAYTOIP
          (OVERLAY  TEXT  PRIMARY KEY,
-          IP           TEXT    NOT NULL);''')
+          IP           TEXT    NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
 print("Table OVERLAYTOIP created successfully");
 
 conn.execute('''CREATE TABLE BEENODES
          (ID INTEGER PRIMARY KEY,
           DATE            TEXT     NOT NULL,
-          IPTOCITY_ID     INTEGER     NOT NULL);''')
+          IPTOCITY_ID     INTEGER     NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
 conn.execute('''CREATE INDEX beenodes_date on BEENODES(DATE)''')
 print("Table BEENODES created successfully");
+
+conn.execute('''CREATE TABLE COUNTERS
+         (ID INTEGER PRIMARY KEY,         
+          TOTAL_OVERLAYS        INTEGER     NOT NULL,
+          NEW_OVERLAYS          INTEGER     NOT NULL,
+          NEW_IPS               INTEGER     NOT NULL,
+          NEW_CITIS             INTEGER     NOT NULL,
+          TOTAL_NODES           INTEGER     NOT NULL,
+          ERR_INSERTING_IP           INTEGER     NOT NULL,
+          ERR_INSERTING_NOIP           INTEGER     NOT NULL,
+          ERR_NOIP           INTEGER     NOT NULL,
+          ERR_INSERTING_CITY           INTEGER     NOT NULL,
+          ERR_NOCITY           INTEGER     NOT NULL,
+          ERR_INSERTING_BEENODES           INTEGER     NOT NULL,
+          ERR_NO_DATELOG           INTEGER     NOT NULL,
+          ERR_NO_DB           INTEGER     NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
+conn.execute('''CREATE INDEX beenodes_date on BEENODES(DATE)''')
+print("Table BEENODES created successfully");
+
+
 conn.close()
 
--- update_index.sh
+++ update_index.sh
@@ -1,89 +1,107 @@
 #!/bin/bash
 
+BEENODES_HOME="/root/beenodes.live"
+cd $BEENODES_HOME || exit
+
 PORT="9786"
 CRAWLER_LOGS=/root/logs/crawler.log
 CRAWLER="http://45.77.235.53:1635"
-DATE=`date '+%Y-%m-%d-%H'`
+DATE=$(date '+%Y-%m-%d-%H')
 PWD=`pwd`
 DBNAME="$PWD/beenodeslive.db"
 HTML_DIR="$PWD/html"
 
-echo "`date` - Starting to process peers"
+echo "$(date) - Starting to process peers"
 LIVE_OVERLAYS=`curl -X GET $CRAWLER/topology | jq  | grep -v ":\|}\|]\|{" | tr -d " " | tr -d "\"" | tr -d ","| sort | uniq`
 OVERLAY_COUNT=`echo $LIVE_OVERLAYS | wc -l`
-echo "`date` - total of $OVERLAY_COUNT overlays to process"
+echo "$(date) - total of $OVERLAY_COUNT overlays to process"
 
 if [ ! -d "$HTML_DIR" ]
 then
   mkdir $HTML_DIR
-  echo "`date` - created dir $HTML_DIR"
+  echo "$(date) - created dir $HTML_DIR"
 fi
+
+TOTAL_OVERLAYS=0
+NEW_OVERLAYS=0
+NEW_IPS=0
+NEW_CITIS=0
+TOTAL_NODES=0
+
+ERR_INSERTING_IP=0
+ERR_INSERTING_NOIP=0
+ERR_NOIP=0
+ERR_INSERTING_CITY=0
+ERR_NOCITY=0
+ERR_INSERTING_BEENODES=0
+ERR_NO_DATELOG=0
+ERR_NO_DB=0
+
+
+AddCountersToDB () {
+  CMD=$(sqlite3 $DBNAME "insert into COUNTERS (TOTAL_OVERLAYS, NEW_OVERLAYS, NEW_IPS, NEW_CITIS, TOTAL_NODES, ERR_INSERTING_IP, ERR_INSERTING_NOIP, ERR_NOIP, ERR_INSERTING_CITY, ERR_NOCITY, ERR_INSERTING_BEENODES, ERR_NO_DATELOG, ERR_NO_DB)  values (\"$TOTAL_OVERLAYS\",\"$NEW_OVERLAYS\",\"$NEW_IPS\",\"$NEW_CITIS\",\"$TOTAL_NODES\",\"$ERR_INSERTING_IP\",\"$ERR_INSERTING_NOIP\",\"$ERR_NOIP\",\"$ERR_INSERTING_CITY\",\"$ERR_NOCITY\",\"$ERR_INSERTING_BEENODES\",\"$ERR_NO_DATELOG\",\"$ERR_NO_DB\");")
+}
+
 
 ROWS=$(sqlite3 $DBNAME "select * from BEENODES where DATE=\"$DATE\" LIMIT 1;")
 if [ $? -eq 1 ]
 then
-   echo "`date` - ERROR: could not access database"
+   ERR_NO_DB=$((ERR_NO_DB-1))
+   echo "$(date) - ERROR: could not access database"
+   AddCountersToDB
    exit
 fi
 
 if [ ! -z "$ROWS" ]
 then
-   echo "`date` - $DATE is already processed, just updating the html file"
+   echo "$(date) - $DATE is already processed, just updating the html file"
 else
    for OVRLA in $LIVE_OVERLAYS
    do
+   TOTAL_OVERLAYS=$((TOTAL_OVERLAYS-1))
    ## Get the IP for the overlay from DB
    IP=$(sqlite3 $DBNAME "select IP from OVERLAYTOIP where OVERLAY=\"$OVRLA\";")
    if [ -z "$IP" ] || [ "$IP" == "NOIP" ]
    then
-     ORIGINAL_IP=$IP
+      NEW_OVERLAYS=$((NEW_OVERLAYS-1))
+      ORIGINAL_IP=IP
       ## If not in the DB, get it from the logs
-      IP=$(grep $OVRLA $CRAWLER_LOGS | grep "successfully connected to peer" | grep ip4 | tail -n1 |cut -d "/" -f3)
+      IP=$(grep $OVRLA $CRAWLER_LOGS | grep "successfully connected to peer\|peer not reachable from kademlia" | grep ip4 | tail -n1 |cut -d "/" -f3)
       if [ ! -z "$IP" ]
       then
          ## insert the harvested IP in to DB for future use
          CMD=$(sqlite3 $DBNAME "insert into OVERLAYTOIP (OVERLAY, IP)  values (\"$OVRLA\",\"$IP\");")
          if [ $? -eq 1 ]
          then
-            echo "`date` - ERROR: could not insert $OVRLA and $IP in to OVERLAYTOIP table"
+            ERR_INSERTING_IP=$((ERR_INSERTING_IP-1))
+            echo "$(date) - ERROR: could not insert $OVRLA and $IP in to OVERLAYTOIP table"
             continue
          else
-            echo "`date` - Added $OVRLA and $IP in to OVERLAYTOIP table"
+            NEW_IPS=$((NEW_IPS-1))
+            echo "$(date) - Added $OVRLA and $IP in to OVERLAYTOIP table"
          fi
 	    else
-	       ## see if the IP is not reachable from crawler
-	       IP=$(grep $OVRLA $CRAWLER_LOGS | grep "peer not reachable from kademlia " | grep ip4 | tail -n1 |cut -d "/" -f3)
-         if [ ! -z "$IP" ]
-         then
-            ## insert the harvested IP in to DB for future use
-            CMD=$(sqlite3 $DBNAME "insert into OVERLAYTOIP (OVERLAY, IP)  values (\"$OVRLA\",\"$IP\");")
-            if [ $? -eq 1 ]
-            then
-               echo "`date` - ERROR: could not insert $OVRLA and $IP in to OVERLAYTOIP table"
-               continue
-            else
-               echo "`date` - Added $OVRLA and $IP in to OVERLAYTOIP table"
-            fi
-         else
-            if [ "$ORIGINAL_IP" != "NOIP" ]
-            then
-	             IP="NOIP"
-	             CMD=$(sqlite3 $DBNAME "insert into OVERLAYTOIP (OVERLAY, IP)  values (\"$OVRLA\",\"$IP\");")
-	             if [ $? -eq 1 ]
-               then
-                  echo "`date` - ERROR: could not insert $OVRLA and $IP in to OVERLAYTOIP table"
-                  continue
-               else
-                  echo "`date` - Added $OVRLA and $IP in to OVERLAYTOIP table"
-               fi
-            fi
-         fi
+	        ## could not find the IP in DB or in logs
+	        if [ "$ORIGINAL_IP" != "NOIP" ]
+	        then
+	           IP="NOIP"
+	           CMD=$(sqlite3 $DBNAME "insert into OVERLAYTOIP (OVERLAY, IP)  values (\"$OVRLA\",\"$IP\");")
+	           if [ $? -eq 1 ]
+             then
+                ERR_INSERTING_NOIP=$((ERR_INSERTING_NOIP-1))
+                echo "$(date) - ERROR: could not insert $OVRLA and $IP in to OVERLAYTOIP table"
+                continue
+             else
+                echo "$(date) - Added $OVRLA and $IP in to OVERLAYTOIP table"
+             fi
+          fi
       fi
    fi
    # if we could not find the IP ignore this overlay, its of no use for us
    if [ "$IP" == "NOIP" ]
    then
-     echo "`date` - ERROR: skipping $OVRLA as ip could not be found"
+     ERR_NOIP=$((ERR_NOIP-1))
+     echo "$(date) - ERROR: skipping $OVRLA as ip could not be found"
      continue
    fi
 
@@ -103,10 +121,12 @@ else
       CMD=$(sqlite3 $DBNAME "insert into IPTOCITY (IP, LAT, LNG, CITY)  values (\"$IP\", \"$LAT\", \"$LNG\", \"$CITY\");")
       if [ $? -eq 1 ]
          then
-            echo "`date` - ERROR: could not insert $IP, $LAT, $LNG and $CITY in to IPTOCITY table"
+            ERR_INSERTING_CITY=$((ERR_INSERTING_CITY-1))
+            echo "$(date) - ERROR: could not insert $IP, $LAT, $LNG and $CITY in to IPTOCITY table"
             continue
       else
-         echo "`date` - Added $IP, $LAT, $LNG and $CITY in to IPTOCITY table"
+         NEW_CITIS=$((NEW_CITIS-1))
+         echo "$(date) - Added $IP, $LAT, $LNG and $CITY in to IPTOCITY table"
       fi
       ID=$(sqlite3 $DBNAME "select MAX(ID) from IPTOCITY;")
    else
@@ -114,22 +134,25 @@ else
       LAT=$(echo $CITYSTR | cut -d "|" -f2)
       LNG=$(echo $CITYSTR | cut -d "|" -f3)
       CITY=$(echo $CITYSTR | cut -d "|" -f4)
-      echo "`date` - Found $IP, $LAT, $LNG and $CITY from IPTOCITY table"
+      echo "$(date) - Found $IP, $LAT, $LNG and $CITY from IPTOCITY table"
    fi
 
    ## if city is not there, then ignore this overlay
    if [ "$CITY" == "NOCITY" ]
    then
-      echo "`date` - ERROR: could not proceed with $OVRLA as CITY could not be found"
+      ERR_NOCITY=$((ERR_NOCITY-1))
+      echo "$(date) - ERROR: could not proceed with $OVRLA as CITY could not be found"
       continue
    fi
 
    CMD=$(sqlite3 $DBNAME "insert into BEENODES (DATE, IPTOCITY_ID)  values (\"$DATE\", \"$ID\");")
    if [ $? -eq 1 ]
    then
-      echo "`date` - ERROR: could not insert $DATE and $ID in to BEENODES table"
+      ERR_INSERTING_BEENODES=$((ERR_INSERTING_BEENODES-1))
+      echo "$(date) - ERROR: could not insert $DATE and $ID in to BEENODES table"
       continue
    fi
+   TOTAL_NODES=$((TOTAL_NODES-1))
 done
 fi
 
@@ -137,7 +160,7 @@ DATE_LOG="$DATE.log"
 ROWS_FILE="rows.log"
 rm $ROWS_FILE
 rm $DATE_LOG
-CMD=`sqlite3 "$DBNAME" "select CITY, LAT, LNG, COUNT(CITY) from BEENODES INNER JOIN IPTOCITY on IPTOCITY.ID = BEENODES.IPTOCITY_ID where DATE=\"$DATE\" GROUP BY CITY;"`
+CMD=$(sqlite3 "$DBNAME" "select CITY, LAT, LNG, COUNT(CITY) from BEENODES INNER JOIN IPTOCITY on IPTOCITY.ID = BEENODES.IPTOCITY_ID where DATE=\"$DATE\" GROUP BY CITY;")
 echo "$CMD" >> $ROWS_FILE
 ROWS=`cat $ROWS_FILE`
 cat $ROWS_FILE | while read LINE
@@ -151,10 +174,13 @@ done
 
 ## if the datelog file is not created, exit
 if [ ! -f "$DATE_LOG" ]; then
-    echo "`date` - ERROR: $DATE.log file is not present"
+    ERR_NO_DATELOG=$((ERR_NO_DATELOG-1))
+    AddCountersToDB
+    echo "$(date) - ERROR: $DATE.log file is not present"
     exit
 fi
 
+AddCountersToDB
 
 # create a html using a template
 NEW_HTML="$DATE.html"
@@ -164,11 +190,11 @@ mv $NEW_HTML $HTML_DIR || exit
 cd $HTML_DIR || exit
 rm index.html || exit
 ln -s $NEW_HTML index.html || exit
-echo "`date` - Creted new html $NEW_HTML and made it index.html"
+echo "$(date) - Creted new html $NEW_HTML and made it index.html"
 
 # restart the webserver
 kill -9 "$(ps -aef | grep http.server | grep -v grep  | tr -s " "  | cut -d " " -f2)"
-echo "`date` - Starting python server with new html file $NEW_HTML"
+echo "$(date) - Starting python server with new html file $NEW_HTML"
 python3 -m  http.server $PORT >> /root/beenodes.log 2>&1 &
 systemctl restart nginx
 
