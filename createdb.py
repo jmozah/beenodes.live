5845fcda09f024dca6c5c88e4d7d8c4e0faf981e
Zahoor Mohamed <zahoor@ethswarm.org>
2021-02-22 13:06:18 +0530

    bug fix in creating tables

--- createdb.py
+++ createdb.py
@@ -36,17 +36,16 @@ conn.execute('''CREATE TABLE COUNTERS
           NEW_IPS               INTEGER     NOT NULL,
           NEW_CITIS             INTEGER     NOT NULL,
           TOTAL_NODES           INTEGER     NOT NULL,
-          ERR_INSERTING_IP           INTEGER     NOT NULL,
-          ERR_INSERTING_NOIP           INTEGER     NOT NULL,
-          ERR_NOIP           INTEGER     NOT NULL,
-          ERR_INSERTING_CITY           INTEGER     NOT NULL,
-          ERR_NOCITY           INTEGER     NOT NULL,
-          ERR_INSERTING_BEENODES           INTEGER     NOT NULL,
-          ERR_NO_DATELOG           INTEGER     NOT NULL,
-          ERR_NO_DB           INTEGER     NOT NULL,
+          ERR_INSERTING_IP      INTEGER     NOT NULL,
+          ERR_INSERTING_NOIP    INTEGER     NOT NULL,
+          ERR_NOIP              INTEGER     NOT NULL,
+          ERR_INSERTING_CITY    INTEGER     NOT NULL,
+          ERR_NOCITY            INTEGER     NOT NULL,
+          ERR_INSERTING_BEENODES  INTEGER     NOT NULL,
+          ERR_NO_DATELOG        INTEGER     NOT NULL,
+          ERR_NO_DB             INTEGER     NOT NULL,
           Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-conn.execute('''CREATE INDEX beenodes_date on BEENODES(DATE)''')
-print("Table BEENODES created successfully");
+print("Table COUNTERS created successfully");
 
 
 conn.close()
