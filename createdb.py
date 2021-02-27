e3eaa8bf3e660167c141d9ab6d2c69036f80ef24
Zahoor Mohamed <zahoor@ethswarm.org>
2021-02-27 23:40:16 +0530

    changed schema for go crawler

--- createdb.py
+++ createdb.py
@@ -5,57 +5,50 @@ import sqlite3
 conn = sqlite3.connect('beenodeslive.db')
 print("Opened database successfully")
 
-conn.execute('''CREATE TABLE IPTOCITY
-         (ID INTEGER PRIMARY KEY,
-          IP             TEXT     NOT NULL,
+conn.execute('''CREATE TABLE IP_INFO
+         (IP TEXT PRIMARY KEY,
           LAT            REAL     NOT NULL,
           LNG            REAL     NOT NULL,
           CITY           TEXT     NOT NULL,
           Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-conn.execute('''CREATE INDEX iptocity_ip on IPTOCITY(IP)''')
-print("Table IPTOCITY created successfully")
+print("Table IP_INFO created successfully")
 
-conn.execute('''CREATE TABLE OVERLAYIPPORT
-         (OVERLAY  TEXT  PRIMARY KEY,
-          IP              TEXT    NOT NULL,
-          PORT            INTEGER NOT NULL,  
-          NOT_RESPONDING  INTEGER NOT NULL,
-          RESPONDING      INTEGER NOT NULL,
-          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-print("Table OVERLAYIPPORT created successfully")
+conn.execute('''CREATE TABLE PEER_INFO (
+          BATCH        TEXT    NOT NULL,
+          OVERLAY      TEXT    NOT NULL,
+          UNDERLAY     TEXT    NOT NULL,
+          PEERS_COUNT  INTEGER NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY(BATCH, OVERLAY));''')
+print("Table PEER_INFO created successfully")
 
-conn.execute('''CREATE TABLE CITYBATCH
-         (ID INTEGER PRIMARY KEY,
-          DATE            TEXT     NOT NULL,
+
+conn.execute('''CREATE TABLE NEIGHBOUR_INFO (
+          BATCH                TEXT    NOT NULL,
+          BASE_OVERLAY         TEXT    NOT NULL,
+          NEIGHBOUR_OVERLAY    TEXT    NOT NULL,
+          NEIGHBOUR_IP4or6     TEXT    NOT NULL,
+          NEIGHBOUR_IP         TEXT    NOT NULL,
+          NEIGHBOUR_PROTOCOL   TEXT    NOT NULL,
+          NEIGHBOUR_PORT       TEXT    NOT NULL,
+          NEIGHBOUR_UNDERLAY   TEXT    NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY(BATCH, BASE_OVERLAY, NEIGHBOUR_OVERLAY));''')
+print("Table NEIGHBOUR_INFO created successfully")
+
+conn.execute('''CREATE TABLE CITY_INFO (
+          BATCH           TEXT     NOT NULL,
+          CITY            TEXT     NOT NULL,
           LAT             REAL     NOT NULL,
           LNG             REAL     NOT NULL,
-          CITY            TEXT     NOT NULL,
           GREEN_COUNT     INTEGER  NOT NULL,
           ORANGE_COUNT    INTEGER  NOT NULL,
           RED_COUNT       INTEGER  NOT NULL,
-          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-conn.execute('''CREATE INDEX citybatch_date on CITYBATCH(DATE)''')
-print("Table CITYBATCH created successfully")
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY(BATCH, CITY));''')
+conn.execute('''CREATE INDEX city_info_date on CITY_INFO(BATCH)''')
+print("Table CITY_INFO created successfully")
 
 
-conn.execute('''CREATE TABLE COUNTERS
-         (DATE                  TEXT PRIMARY KEY,         
-          TOTAL_PEERS           INTEGER     NOT NULL,
-          CONNECTED_PEERS       INTEGER     NOT NULL,
-          CONNECTED_FOUND_PEERS INTEGER     NOT NULL,
-          DISCONNECTED_PEERS    INTEGER     NOT NULL,
-          NEW_PEERS             INTEGER     NOT NULL,
-          NEW_IPS               INTEGER     NOT NULL,
-          NEW_CITIS             INTEGER     NOT NULL,
-          TOTAL_ROWS_IN_BATCH   INTEGER     NOT NULL,          
-          ERR_SELECTING_IP      INTEGER     NOT NULL,
-          ERR_INSERTING_IP      INTEGER     NOT NULL,
-          ERR_NOIP              INTEGER     NOT NULL,
-          ERR_SELECTING_CITY    INTEGER     NOT NULL,
-          ERR_INSERTING_CITY    INTEGER     NOT NULL,
-          ERR_NOCITY            INTEGER     NOT NULL,
-          ERR_INSERTING_CITYBATCH  INTEGER     NOT NULL,
-          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-print("Table COUNTERS created successfully")
 conn.close()
 
--- upgradedb.py
+++ upgradedb.py
@@ -12,55 +12,82 @@ print("Dropped table  BEENODES successfully")
 conn.execute('''DROP TABLE IF EXISTS COUNTERS''')
 print("Dropped table  COUNTERS successfully")
 
-conn.execute('''DROP INDEX IF EXISTS beenodes_date''')
-print("Dropped index  beenodes_date successfully")
-
 conn.execute('''DROP TABLE IF EXISTS CITYBATCH''')
 print("Dropped table CITYBATCH successfully")
 
 conn.execute('''DROP TABLE IF EXISTS OVERLAYIPPORT''')
 print("Dropped table OVERLAYIPPORT successfully")
 
-conn.execute('''CREATE TABLE OVERLAYIPPORT
-         (OVERLAY  TEXT  PRIMARY KEY,
-          IP           TEXT    NOT NULL,
-          PORT         INTEGER NOT NULL,
-          NOT_RESPONDING  INTEGER NOT NULL,
-          RESPONDING  INTEGER NOT NULL,
+conn.execute('''DROP INDEX IF EXISTS beenodes_date''')
+print("Dropped index  beenodes_date successfully")
+
+conn.execute('''DROP INDEX IF EXISTS citybatch_date''')
+print("Dropped index  citybatch_date successfully")
+
+conn.execute('''CREATE TABLE IP_INFO
+         (IP TEXT PRIMARY KEY,
+          LAT            REAL     NOT NULL,
+          LNG            REAL     NOT NULL,
+          CITY           TEXT     NOT NULL,
           Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-print("Table OVERLAYIPPORT created successfully")
+print("Table IP_INFO created successfully")
+
+print('moving data from IPTOCITY to IP_INFO')
+cursor = conn.execute('''SELECT IP, CITY, LAT, LNG FROM  IPTOCITY''')
+ip_info = dict()
+for row in cursor:
+    ip = row[0]
+    city = row[1]
+    lat = row[2]
+    lng = row[3]
+    ip_info[ip] = (city, lat, lng)
+count = 0
+for ip in ip_info:
+    (city, lat, lng) = ip_info[ip]
+    conn.execute('insert into IP_INFO (IP, CITY, LAT, LNG) values(?, ?, ?, ?)', (ip, city, lat, lng))
+    count += 1
+conn.commit()
+print('moved {} rows from IPTOCITY to IP_INFO'.format(str(count)))
+
+conn.execute('''DROP TABLE IF EXISTS IPTOCITY''')
+print("Dropped table IPTOCITY successfully")
+
+conn.execute('''CREATE TABLE PEER_INFO (
+          BATCH        TEXT    NOT NULL,
+          OVERLAY      TEXT    NOT NULL,
+          UNDERLAY     TEXT    NOT NULL,
+          PEERS_COUNT  INTEGER NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY(BATCH, OVERLAY));''')
+print("Table PEER_INFO created successfully")
 
 
-conn.execute('''CREATE TABLE CITYBATCH
-         (ID INTEGER PRIMARY KEY,
-          DATE            TEXT     NOT NULL,
+conn.execute('''CREATE TABLE NEIGHBOUR_INFO (
+          BATCH                TEXT    NOT NULL,
+          BASE_OVERLAY         TEXT    NOT NULL,
+          NEIGHBOUR_OVERLAY    TEXT    NOT NULL,
+          NEIGHBOUR_IP4or6     TEXT    NOT NULL,
+          NEIGHBOUR_IP         TEXT    NOT NULL,
+          NEIGHBOUR_PROTOCOL   TEXT    NOT NULL,
+          NEIGHBOUR_PORT       TEXT    NOT NULL,
+          NEIGHBOUR_UNDERLAY   TEXT    NOT NULL,
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY(BATCH, BASE_OVERLAY, NEIGHBOUR_OVERLAY));''')
+print("Table NEIGHBOUR_INFO created successfully")
+
+
+conn.execute('''CREATE TABLE CITY_INFO (
+          BATCH           TEXT     NOT NULL,
+          CITY            TEXT     NOT NULL,
           LAT             REAL     NOT NULL,
           LNG             REAL     NOT NULL,
-          CITY            TEXT     NOT NULL,
           GREEN_COUNT     INTEGER  NOT NULL,
           ORANGE_COUNT    INTEGER  NOT NULL,
           RED_COUNT       INTEGER  NOT NULL,
-          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-conn.execute('''CREATE INDEX citybatch_date on CITYBATCH(DATE)''')
-print("Table CITYBATCH created successfully")
+          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY(BATCH, CITY));''')
+conn.execute('''CREATE INDEX city_info_date on CITY_INFO(BATCH)''')
+print("Table CITY_INFO created successfully")
 
-conn.execute('''CREATE TABLE COUNTERS
-         (DATE                  TEXT PRIMARY KEY,         
-          TOTAL_PEERS           INTEGER     NOT NULL,
-          CONNECTED_PEERS       INTEGER     NOT NULL,
-          CONNECTED_FOUND_PEERS INTEGER     NOT NULL,
-          DISCONNECTED_PEERS    INTEGER     NOT NULL,
-          NEW_PEERS             INTEGER     NOT NULL,
-          NEW_IPS               INTEGER     NOT NULL,
-          NEW_CITIS             INTEGER     NOT NULL,
-          TOTAL_ROWS_IN_BATCH   INTEGER     NOT NULL,          
-          ERR_SELECTING_IP      INTEGER     NOT NULL,
-          ERR_INSERTING_IP      INTEGER     NOT NULL,
-          ERR_NOIP              INTEGER     NOT NULL,
-          ERR_SELECTING_CITY    INTEGER     NOT NULL,
-          ERR_INSERTING_CITY    INTEGER     NOT NULL,
-          ERR_NOCITY            INTEGER     NOT NULL,
-          ERR_INSERTING_CITYBATCH  INTEGER     NOT NULL,
-          Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-print("Table COUNTERS created successfully")
+
 conn.close()
