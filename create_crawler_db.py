3138bbc7b858586c29977f7ffc2816a5a8ce7c7c
Zahoor Mohamed <zahoor@ethswarm.org>
2021-02-28 12:19:54 +0530

    changed crawler schema

--- create_crawler_db.py
+++ create_crawler_db.py
@@ -5,6 +5,10 @@ print("Opened database successfully")
 
 conn.execute('''CREATE TABLE PEER_INFO (
           OVERLAY      TEXT    NOT NULL,
+          IP4or6       TEXT    NOT NULL,
+          IP           TEXT    NOT NULL,
+          PROTOCOL     TEXT    NOT NULL,
+          PORT         TEXT    NOT NULL,
           UNDERLAY     TEXT    NOT NULL,
           PEERS_COUNT  INTEGER NOT NULL,
           Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -15,17 +19,10 @@ print("Table PEER_INFO created successfully")
 conn.execute('''CREATE TABLE NEIGHBOUR_INFO (
           BASE_OVERLAY         TEXT    NOT NULL,
           PROXIMITY_ORDER      INTEGER NOT NULL,
-          NEIGHBOUR_OVERLAY    TEXT    NOT NULL,
-          NEIGHBOUR_IP4or6     TEXT    NOT NULL,
-          NEIGHBOUR_IP         TEXT    NOT NULL,
-          NEIGHBOUR_PROTOCOL   TEXT    NOT NULL,
-          NEIGHBOUR_PORT       TEXT    NOT NULL,
-          NEIGHBOUR_UNDERLAY   TEXT    NOT NULL,
+          NEIGHBOUR_OVERLAY   TEXT    NOT NULL,
           Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
           PRIMARY KEY(BASE_OVERLAY, NEIGHBOUR_OVERLAY));''')
 print("Table NEIGHBOUR_INFO created successfully")
-conn.execute('''CREATE INDEX neighbour_info_neighbour_overlay_date on NEIGHBOUR_INFO(NEIGHBOUR_OVERLAY)''')
-print("Index neighbour_info_neighbour_overlay_date created successfully")
 
 conn.close()
 
